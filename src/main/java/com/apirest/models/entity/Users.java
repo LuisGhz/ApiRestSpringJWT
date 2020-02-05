218fc08f21edaacdb79ceb4ff34074e55d7aa3a4
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-02-04 22:15:36 -0600

    Users entity started

--- src/main/java/com/apirest/models/entity/Users.java
+++ src/main/java/com/apirest/models/entity/Users.java
@@ -1,5 +1,121 @@
 package com.apirest.models.entity;
 
-public class Users {
+import java.io.Serializable;
+import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUsers;
+
+	private String username;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Date birthDate;
+	
+	private byte age;
+	
+	private String email;
+	
+	private String gender;
+	
+	private String picture;
+	
+	private boolean isActive;
+
+	public Long getIdUsers() {
+		return idUsers;
+	}
+
+	public void setIdUsers(Long idUsers) {
+		this.idUsers = idUsers;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public void setAge(byte age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
 }
